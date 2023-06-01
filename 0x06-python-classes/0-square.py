ccacbf1b04cf9f4f3a9de492ffd3b7e2f4c5b5de
Ronald116 <ronaldbotchway@gmail.com>
2023-05-31 20:42:43 -0700

    0x06-python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''
+Define a square class
+'''
+
+class Square:
+    '''
+    Does nothing
+    '''
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''defines a class square'''
+
+class Square:
+    '''
+    initialize square value
+
+    Args:
+        size - size of the square
+    '''
+
+    self.__size = size

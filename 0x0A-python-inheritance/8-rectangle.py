80a4c4443d19361d93c3c292792a4b47f060c023
Ronald116 <ronaldbotchway@gmail.com>
2023-06-12 11:23:51 -0700

    COMPLETED TASKS

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """A child class that inherits parent class"""
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validotor("height", height)

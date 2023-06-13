345227be97e07423ea798c236b5370eccb5fb677
Ronald116 <ronaldbotchway@gmail.com>
2023-06-13 11:47:55 -0700

    UPDATED

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
+""" a class Student"""
+
+
 class Student:
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        class_d = self.__dict__
-        sel_d = dict()
+        """Get a dictionary representation of the Student
 
-        if type(attrs) is list:
-            for attr in attrs:
-                if type(attr) is not str:
-                    return class_d
-
-                if attr in class_d:
-                    sel_d[attr] = class_d[attr]
-
-            return sel_d
-
-        return class_d
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__

77d8bda82e3292f11e99644fa9935ca2a17e13ec
Ronald116 <ronaldbotchway@gmail.com>
2023-06-13 13:27:55 -0700

    TASK 10

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""Subclass MyInt inherits parent Int"""
 
 class MyInt(int):
+    """invert int operators"""
+
     def __eq__(self, other):
+        """replace == with !="""
         return super().__ne__(other)
 
     def __ne__(self, other):
+        """replace != with =="""
         return super().__eq__(other)

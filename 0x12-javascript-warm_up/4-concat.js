569412ba6663ee8fd5493a038002de14ec9ad255
Ronald116 <ronaldbotchway@gmail.com>
2023-08-14 08:34:46 -0700

    update

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
+const argv0 = process.argv[2];
+const argv1 = process.argv[3];
 
-if (process.argv[2] && process.argv[3]) {
-    console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
+console.log(`${argv0} is ${argv1}`);
